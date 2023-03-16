b86c3b3394e2b4e156f366c19ec7987f9600042c
ritesh <riteshraj1064@gmail.com>
2023-03-16 21:14:14 +0530

    host

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "https://strapi-n3pc.onrender.com"),
+  host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
